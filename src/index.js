f46f8ad0593af000debb76de8830bafd8bdd9715
AMV1909 <amorales264@unab.edu.co>
2022-07-30 11:57:59 -0500

    First Commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import { dirname, join } from "path"
+import { fileURLToPath } from "url"
+
+import indexRoutes from "./routes/index.js"
+
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// Settings
+app.set("port", process.env.PORT || 3000)
+app.set("views", join(__dirname, "views"))
+app.set("view engine", "ejs")
+app.use(express.static(join(__dirname, "public")))
+
+// Middlewares
+
+
+// Routes
+app.use(indexRoutes)
+
+// Starting the server
+app.listen(app.get("port"), () => console.log("Server on port", app.get("port")))
